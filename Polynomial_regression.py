648ed031ec1205dcdb27e3579090765f776030cb
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-02-07 10:11:48 -0500

    Update Polynomial_regression.py

--- Polynomial_regression.py
+++ Polynomial_regression.py
@@ -22,6 +22,7 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
+# no need scaling for polynomial regression.. regressor does it
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
